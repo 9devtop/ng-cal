213e4ebba83ec0eeea4357c6a26789374e268f84
Tan <tan@think@.co.nz>
2018-12-06 11:24:09 +0700

    add API calling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'NineDevDemo';
+  title : string = 'NineDevDemo';
+  ketqua : number = 0;
+
+  add(a: number, b: number):void {
+    this.ketqua = a + b;
+    console.log(`${a} + ${b} = ${this.ketqua}`);
+  }
+
+  // To call external API
+  // constructor(private http: HttpClient){}
+  // add(a: number, b: number):void {
+  //   this.http.get<number>(`https://localhost:44371/api/values/cong/${a}/${b}`).subscribe(kq => {
+  //     this.ketqua = kq;
+  //     console.log(`API ${a} + ${b} = ${this.ketqua}`);
+  //   });
+  // }
 }
